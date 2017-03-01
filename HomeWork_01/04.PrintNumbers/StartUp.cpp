f6ef73918792caadbaf53a4c951bb4462b6b9f1a
Ivan Trendafilov <ivan.trendafiloff@gmail.com>
2017-03-01 15:03:59 +0200

    Added exercise 04/05

---
+++ HomeWork_01/04.PrintNumbers/StartUp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (size_t i = 1; i <= N; i++)
+	{
+		cout << i << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+}
