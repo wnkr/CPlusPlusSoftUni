8dda02b06a5d91b18276f2f156403372704b7fdb
Ivan Trendafilov <ivan.trendafiloff@gmail.com>
2017-05-20 17:34:08 +0300

    added C++11 list-initialization of elements to the List

--- HomeWork_09_C11_14AdvancedFeatures/01.TemplateListWithRangeLoop/main.cpp
+++ HomeWork_09_C11_14AdvancedFeatures/01.TemplateListWithRangeLoop/main.cpp
@@ -4,44 +4,15 @@
 
 #include "List.h"
 
-template<typename T>
-List<T> mergeSortedLists(List<T> a, List<T> b) {
-    List<T> merged;
-
-    while(!a.isEmpty() || !b.isEmpty()) {
-        List<T> * listToTakeFrom;
-        if (a.isEmpty()) {
-            listToTakeFrom = &b;
-        } else if (b.isEmpty()) {
-            listToTakeFrom = &a;
-        } else {
-            if (a.first() < b.first()) {
-                listToTakeFrom = &a;
-            } else {
-                listToTakeFrom = &b;
-            }
-        }
-
-        merged.add(listToTakeFrom->first());
-        listToTakeFrom->removeFirst();
-    }
-
-    return merged;
-}
-
 int main() {
-	List<int> numbers;
-	numbers.add(1);
-	numbers.add(-2);
-	numbers.add(42);
+	
+	List<int> numbers{ 1, -2, 42 };
 	for (int n : numbers) {
 		std::cout << n << " ";
 	}
 	std::cout << std::endl;
 
-	List<std::string> words;
-	words.add("Guardians");
-	words.add("galaxy");
+	List<std::string> words{ "Guardians", "galaxy" };
 	for (auto word : words) {
 		std::cout << word << " ";
 	}
